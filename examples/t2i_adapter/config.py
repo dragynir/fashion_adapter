ee3f73b2dc963a013f27cf86e6bb854f9334998a
dragynir <d.korostelevdn@gmail.com>
2023-12-29 19:38:57 +0700

    Add training config

--- examples/t2i_adapter/config.py
+++ examples/t2i_adapter/config.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class TrainingConfig:
     pretrained_model_name_or_path: str
@@ -19,21 +20,22 @@ class TrainingConfig:
     report_to: str
 
     seed: int
-    def __post_init__(self):
-        self.pretrained_model_name_or_path = 'stabilityai/stable-diffusion-xl-base-1.0'
-        self.output_dir = './training_logs'
-        self.dataset_name = './fill50k'
 
-        self.mixed_precision = 'fp16'
-        self.resolution = 1024
-        self.learning_rate = 1e-5
-        self.max_train_steps = 15000
-        self.train_batch_size = 1
-        self.gradient_accumulation_steps = 4
 
-        self.validation_image = './validation/conditioning_image_1.png" "./validation/conditioning_image_2.png'
-        self.validation_prompt = 'red circle with blue background" "cyan circle with brown floral background'
-        self.validation_steps = 100
-        self.report_to = 'tensorboard'
+training_config = TrainingConfig(
+    pretrained_model_name_or_path='stabilityai/stable-diffusion-xl-base-1.0',
+    output_dir='./training_logs',
+    dataset_name='./fill50k',
+    mixed_precision='fp16',
+    resolution=1024,
+    learning_rate=1e-5,
+    max_train_steps=15000,
+    train_batch_size=1,
+    gradient_accumulation_steps=4,
 
-        self.seed = 42
+    validation_image='./validation/conditioning_image_1.png" "./validation/conditioning_image_2.png',
+    validation_prompt='red circle with blue background" "cyan circle with brown floral background',
+    validation_steps=100,
+    report_to='tensorboard',
+    seed=42,
+)
